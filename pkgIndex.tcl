c6fa905d975d2234506f1da3196ecec83e0abcd4
TheMarkitecht <Mark@TheMarkitecht.com>
2018-06-01 17:14:30 -0600

    Enabled 'package require MityBuild' instead of 'source'.

---
+++ pkgIndex.tcl
@@ -0,0 +1,10 @@
+# This script configures Tcl interp to find and load libMityBuild.
+
+# For this to work, user should have set his environment TCLLIBPATH
+# to the directory where pkgIndex.tcl is stored.
+# That way requires no modifications to the operating system directories by the root user.
+
+package ifneeded MityBuild 3.0 {
+    tcl_findLibrary MityBuild 3.0 0 libMityBuild {} MityBuildDir
+}
+
