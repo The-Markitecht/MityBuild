bb86cc2bf5b2809b20b72a5e1431ff46476d5173
TheMarkitecht <Mark@TheMarkitecht.com>
2018-10-05 18:05:30 -0500

    Fix error 'unknown command ::MityBuild::pkgconfig' when using 'package require' in a SSH from Windows to Laptop.

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -5,6 +5,7 @@
 # That way requires no modifications to the operating system directories by the root user.
 
 package ifneeded MityBuild 3.0 {
+    interp alias {} ::MityBuild::pkgconfig {} ::tcl::pkgconfig
     tcl_findLibrary MityBuild 3.0 0 libMityBuild {} MityBuildDir
 }
 
